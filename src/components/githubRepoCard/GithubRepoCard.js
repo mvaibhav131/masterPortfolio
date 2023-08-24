8ba46e86c76137c22b03dfb3bc10d4a3fb156536
Vaibhav More <98808183+mvaibhav131@users.noreply.github.com>
2023-08-24 18:14:05 +0530

    edit projects

--- src/components/githubRepoCard/GithubRepoCard.js
+++ src/components/githubRepoCard/GithubRepoCard.js
@@ -33,7 +33,7 @@ export default function GithubRepoCard({ repo, theme }) {
               className="repo-creation-date subTitle"
               style={{ color: theme.secondaryText }}
             >
-              Created on {repo.created_at}
+              Created on {repo.createdAt.split("T")[0]}
             </p>
           </div>
           <div className="repo-languages">
