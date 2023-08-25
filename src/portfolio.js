da021ca318cf12c1447963dde6c7f7fa55c4f571
Vaibhav More <98808183+mvaibhav131@users.noreply.github.com>
2023-08-25 16:09:50 +0530

    publication

--- src/portfolio.js
+++ src/portfolio.js
@@ -807,30 +807,30 @@ const projectsHeader = {
 };
 
 const publicationsHeader = {
-  // title: "Publications",
-  // description:
-  //   "I have worked on and published a few research papers and publications of my own.",
-  // avatar_image_path: "projects_image.svg",
+  title: "Publications",
+  description:
+    "I have worked on and published a few research papers and publications of my own.",
+  avatar_image_path: "projects_image.svg",
 };
 
 const publications = {
   data: [
-    // {
-    //   id: "MDEwOlJlcG9zaXRvcnkyNDU0NjcyNzQ=",
-    //   name: "Artificial Intelligence Paper",
-    //   createdAt: "2020-03-06T16:26:54Z",
-    //   description: "Paper Written on Artificial Intelligence published in xyz ",
-    //   url:
-    //     "https://www.andrewng.org/publications/building-high-level-features-using-large-scale-unsupervised-learning/",
-    // },
-    // {
-    //   id: "MDEwOlJlcG9zaXRvcnkyNDU0NjcyNzi=",
-    //   name: "Artificial Intelligence Paper",
-    //   createdAt: "2020-03-06T16:26:54Z",
-    //   description: "Paper Written on Artificial Intelligence published in xyz ",
-    //   url:
-    //     "https://www.andrewng.org/publications/building-high-level-features-using-large-scale-unsupervised-learning/",
-    // },
+    {
+      id: "MDEwOlJlcG9zaXRvcnkyNDU0NjcyNzQ=",
+      name: "Artificial Intelligence Paper",
+      createdAt: "2020-03-06T16:26:54Z",
+      description: "Paper Written on Artificial Intelligence published in xyz ",
+      url:
+        "https://www.andrewng.org/publications/building-high-level-features-using-large-scale-unsupervised-learning/",
+    },
+    {
+      id: "MDEwOlJlcG9zaXRvcnkyNDU0NjcyNzi=",
+      name: "Artificial Intelligence Paper",
+      createdAt: "2020-03-06T16:26:54Z",
+      description: "Paper Written on Artificial Intelligence published in xyz ",
+      url:
+        "https://www.andrewng.org/publications/building-high-level-features-using-large-scale-unsupervised-learning/",
+    },
     // {
     //   id: "MDEwOlJlcG9zaXRvcnkyNDU0NjcyNze=",
     //   name: "Artificial Intelligence Paper",
